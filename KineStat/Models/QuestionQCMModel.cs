213c87cc7007dab389c9330b04c0df263c8bd099
Sacha Meunier <Sacha.Meunier@student.hers.be>
2025-10-27 10:03:33 +0100

    feat(QuestionQCMModel) : add new class QuestionQCMModel

---
+++ KineStat/Models/QuestionQCMModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static KineStat.Models.QuestionModel;
+
+namespace KineStat.Models
+{
+    /// <summary>
+    /// Question de type QCM (Questionnaire à Choix Multiples)
+    /// </summary>
+    [Table("QuestionQCM")]
+    public class QuestionQCM : Question
+    {
+        public QuestionQCM()
+        {
+            Type = "QCM";
+            ListeAnswers = new List<string>();
+        }
+
+        [Required]
+        [NotMapped] 
+        public List<string> ListeAnswers { get; set; }
+
+       
+
+        public override bool Validate()
+        {
+            if (Answer == null || string.IsNullOrEmpty(Answer.Valeur))
+                return false;
+
+            return ListeAnswers.Contains(Answer.Valeur);
+        }
+    }
+}
