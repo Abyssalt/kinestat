846f20c1e7fd8337f19431f870cd4b2558606ee6
Joanna <joanna.imjalli@student.hers.be>
2025-11-22 18:18:46 +0100

    feat (models): add MedicalRecord entity to store patient assessment history

---
+++ KineStat/Models/MedicalRecord.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KineStat.Models
+{
+    public class MedicalRecord //This class contains every assessment of a patient
+    {
+        [Key]
+        public int Id { get; set; }
+        public int PatientId {  get; set; }
+        public virtual ICollection<Assessment> Assessments { get; set; }
+        public bool IsArchived { get; set; }
+        [Required, DataType(DataType.Date), Column(TypeName = "ArchivedDate")]
+        public DateTime ArchivedAt { get; set; }
+    }
+}
