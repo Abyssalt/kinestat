6b7e59f9a0cf96e6b7bc0b2a271859f73e61b75c
Joanna <joanna.imjalli@student.hers.be>
2025-11-22 18:17:01 +0100

    feat (models) : add ClinicalData model for patient clinical profile

---
+++ KineStat/Models/ClinicalData.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KineStat.Models
+{
+    public class ClinicalData //This class is used to store values of each axe of the patient's clinical profile
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int PatientId { get; set; }
+        public int CategoryId { get; set; }
+        public double Value { get; set; }
+
+        public virtual Patient Patient { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
