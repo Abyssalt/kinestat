8a923d54b0847977ebc59f802f7642cf20f60a82
Joanna <joanna.imjalli@student.hers.be>
2025-11-22 18:19:32 +0100

    feat (models) : add PriorContext to store priore per category and medical context

---
+++ KineStat/Models/PriorContext.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KineStat.Models
+{
+    public class PriorContext // This class represents the prior of each category according to the current medical context
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public double Value { get; set; } = 0;
+
+        public int CategoryId;
+
+        public int MedicalContextId;
+
+        public virtual MedicalContext MedicalContext { get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}
