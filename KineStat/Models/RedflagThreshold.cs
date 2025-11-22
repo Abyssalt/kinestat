b3a0c1918b3306f172f9a41af8cc5cd85665f1ff
Joanna <joanna.imjalli@student.hers.be>
2025-11-22 18:20:20 +0100

    feat (models) : add RedflagThreshold to define severity thresholds

---
+++ KineStat/Models/RedflagThreshold.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KineStat.Models
+{
+    public class RedflagThreshold
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public double ThresholdPercentage { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+
+
+    }
+}
