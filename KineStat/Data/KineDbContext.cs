cfffd0ad97c7781e7a7f0871c151f65e48d7d42f
Joanna <joanna.imjalli@student.hers.be>
2025-11-22 18:46:20 +0100

    refactor(dbcontext): pluralize DbSets and add new entities

--- KineStat/Data/KineDbContext.cs
+++ KineStat/Data/KineDbContext.cs
@@ -10,18 +10,26 @@ namespace KineStat.Data
         }
 
         public DbSet<Patient> Patients { get; set; }
-        public DbSet<Physio> Physio { get; set; }
+        public DbSet<Physio> Physios { get; set; }
         public DbSet<Cluster> Cluster { get; set; }
-        public DbSet<Bilan> Bilan { get; set; }
-        public DbSet<QuestionBool> QuestionBool { get; set; }
-        public DbSet<QuestionQCM> QuestionQCM { get; set; }
-        public DbSet<QuestionLadder> QuestionLadder { get; set; }
-        public DbSet<AnswerBool> AnswerBool { get; set; }
-        public DbSet<AnswerQCM> AnswerQCM { get; set; }
-        public DbSet<AnswerLadder> AnswerLadder { get; set; }
-        public DbSet<Redflag> Redflag { get; set; }
-        public DbSet<Pathology> Pathology { get; set; }
-        public DbSet<Category> Category { get; set; }
+        public DbSet<Assessment> Assessments { get; set; }
+        public DbSet<QuestionBool> QuestionBools { get; set; }
+        public DbSet<QuestionQCM> QuestionQCMs { get; set; }
+        public DbSet<QuestionLadder> QuestionLadders { get; set; }
+        public DbSet<Redflag> Redflags { get; set; }
+        public DbSet<Pathology> Pathologies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet <Answer> Answers { get; set; }
+        public DbSet <ClinicalData> ClinicalDatas { get; set; }
+        public DbSet<MedicalContext> MedicalContexts { get; set; }
+        public DbSet<MedicalRecord> MedicalRecords { get; set; }
+        public DbSet<PatientAnswerBool> PatientAnswerBools { get; set; }
+        public DbSet<PatientAnswerNumeric> PatientAnswerNumerics { get; set; }
+        public DbSet<PatientAnswerQCM> PatientAnswerQCMs { get; set; }
+        public DbSet<PriorContext> PriorContexts { get; set; }
+        public DbSet<Socrate> Socrates { get; set; }
+        public DbSet<RedflagThreshold> RedflagThresholds { get; set; }
+
 
 
     }
