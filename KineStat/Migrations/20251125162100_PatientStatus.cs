76f7546a882f93d6c5aab2a9004be6bf20d58aa8
Sacha Meunier <Sacha.Meunier@student.hers.be>
2025-11-25 17:21:45 +0100

    feat(migration) : add new migration

---
+++ KineStat/Migrations/20251125162100_PatientStatus.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KineStat.Migrations
+{
+    /// <inheritdoc />
+    public partial class PatientStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PatientStatus",
+                table: "Patients",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PatientStatus",
+                table: "Patients");
+        }
+    }
+}
