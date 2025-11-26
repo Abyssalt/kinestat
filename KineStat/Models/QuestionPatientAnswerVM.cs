30e8b104c62aef3c3396c3b0132b14a08a422454
Joanna <joanna.imjalli@student.hers.be>
2025-11-26 19:14:20 +0100

    feat: add QuestionPatientAnswerVM to share question and patient answer in views     this viweModel encapsulates a question, the associated patient answer, and helper methods to access comments

---
+++ KineStat/Models/QuestionPatientAnswerVM.cs
@@ -0,0 +1,31 @@
+﻿namespace KineStat.Models
+{
+    //This class is used as a ViewModel to share information between the controller and View
+    public class QuestionPatientAnswerVM 
+    {
+        public int PatientId { get; set; }
+        public Question Question { get; set; }
+
+        public PatientAnswer Answer { get; set; }
+
+
+        public string GetComment()
+        {
+            if (Answer == null || Answer.Comment == null)
+            {
+                return "";
+            }
+            return Answer.Comment;
+        }
+
+        public double GetRvPositive()
+        {
+            return Question.RVPositive;
+        }
+        public double GetRvNegative()
+        {
+            return Question.RVNegative;
+        }
+
+    }
+}
