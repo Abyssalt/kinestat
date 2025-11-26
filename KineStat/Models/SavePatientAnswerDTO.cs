25afe91ced3aecbdf42275a7dda5c921ee0a5713
Joanna <joanna.imjalli@student.hers.be>
2025-11-26 19:16:16 +0100

    feat: add SavePatientAnswerDTO to transfer patient answers from view to controller

---
+++ KineStat/Models/SavePatientAnswerDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace KineStat.Models
+{
+    /// <summary>
+    /// This class is a DTO  used to pass patient responses from the view to the controller
+    /// Supports different types of answers: boolean, numeric, and optional comments
+    /// </summary>
+    public class SavePatientAnswerDTO
+    {
+        public int PatientId { get; set; }
+        public int QuestionId { get; set; }
+        public bool ? BoolValue { get; set; }
+        public double ? NumericValue { get; set; }
+        public string ? Comment { get; set;  }
+    }
+}
