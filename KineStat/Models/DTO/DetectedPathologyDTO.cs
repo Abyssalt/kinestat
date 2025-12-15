5d9257203853bdf0226794803d86fcbe6670a904
Joanna <joanna.imjalli@student.hers.be>
2025-12-15 19:45:17 +0100

    feat : add DetectedPathologyDTO to transfer patient pathology name and probability

---
+++ KineStat/Models/DTO/DetectedPathologyDTO.cs
@@ -0,0 +1,12 @@
+﻿
+namespace KineStat.Models.DTO
+{
+    public class DetectedPathologyDTO
+    {
+        public int PatientId { get; set; }
+        public int AssessmentId { get; set; }
+        public int PathologyId { get; set; }
+        public string PathologyName { get; set; }
+        public double PathologyPercentage { get; set; } 
+    }
+}
