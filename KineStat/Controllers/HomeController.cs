7cb919dd1acc0400f28730dd53e4952da6282b29
Ryan <ryan.wilmart@student.hers.be>
2025-12-15 07:05:31 +0100

    test(error): add temporary routes to test error pages

--- KineStat/Controllers/HomeController.cs
+++ KineStat/Controllers/HomeController.cs
@@ -34,5 +34,18 @@ namespace KineStat.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        // Used for testing error pages
+        [HttpGet]
+        public IActionResult TestAccessDenied()
+        {
+            return RedirectToAction("AccessDenied", "Error");
+        }
+
+        [HttpGet]
+        public IActionResult TestUnauthorized()
+        {
+            return RedirectToAction("Unauthorized", "Error");
+        }
     }
 }
