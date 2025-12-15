06e61ea067155f247c40f6ff3d8ea3dbb0c4f141
Joanna <joanna.imjalli@student.hers.be>
2025-12-15 19:43:48 +0100

    feat : add PatientPathologiesDetected model to store patient pathology probabilities per assessment

---
+++ KineStat/Models/PatientPathologiesDetected.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KineStat.Models
+{
+    public class PatientPathologiesDetected
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int PatientId { get; set; }
+        [Required]
+        public int AssessmentId { get; set; }  
+        [Required]
+        public int PathologyId { get; set; }    
+        [Required]
+        public double PathologyProbability { get; set; }    
+
+        public Pathology Pathology { get; set; }
+        public Patient Patient { get; set; }
+        public Dossier Dossier { get; set; }
+
+    }
+}
