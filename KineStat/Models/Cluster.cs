28333bd360d5b57e578cb890e330c28830d514e7
Joanna <joanna.imjalli@student.hers.be>
2025-12-16 22:47:56 +0100

    feat (cluster) : add RvPositive, RvNegative, MinNumberOfPositiveTests to Cluster

--- KineStat/Models/Cluster.cs
+++ KineStat/Models/Cluster.cs
@@ -12,7 +12,17 @@ namespace KineStat.Models
 
         public string Description { get; set; }
 
+        [Required]
+        public int MinNumberOfPositiveTests { get; set; }
+        
+        [Required]
+        public int RVPositive {  get; set; }
+        [Required]
+        public int RVNegative { get; set; }
+
         public List<Question> Questions { get; set; } = new List<Question>();
+
+        public List<Pathology> ? Pathologies  { get; set; }
 
     }
 }
